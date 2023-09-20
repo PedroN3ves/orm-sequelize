09a71a44afbead37fa82452507c3a221fc5e6775
PedroN3ves <pedroholiveira784@gmail.com>
2023-09-20 11:52:03 -0300

    Code style applied

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,27 +1,25 @@
-const User = require("../models/User");
+const User = require('../models/User');
 
 module.exports = {
-    async index(req, res){
-        const users = await User.findAll({raw: true});
+  async index(req, res) {
+    const users = await User.findAll({ raw: true });
 
-        return res.json(users);
-    },
+    return res.json(users);
+  },
 
-    async store(req, res) {
-        const { name, email } = req.body;
+  async store(req, res) {
+    const { name, email } = req.body;
 
-        const user = await User.create({name, email});
-        return res.json(user);
-    },
+    const user = await User.create({ name, email });
+    return res.json(user);
+  },
 
-    async update(req, res){
-        const { id } = req.params;
-        const { name, email } = req.body;
+  async update(req, res) {
+    const { id } = req.params;
+    const { name, email } = req.body;
 
-        const user = await User.update(
-            { name, email }, { where: {id: id} }
-        );
+    const user = await User.update({ name, email }, { where: { id } });
 
-        return res.json(user); 
-    }
-}
+    return res.json(user);
+  },
+};
--- src/database/migrations/20230915130319-create-users.js
+++ src/database/migrations/20230915130319-create-users.js
@@ -1,35 +1,35 @@
-"use strict";
+// "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable('users', {
-      id:{
+      id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
-        allowNull: true
+        allowNull: true,
       },
-      name:{
+      name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
-      email:{
+      email: {
         type: Sequelize.STRING,
-        allowNull:false
+        allowNull: false,
       },
-      create_at:{
-        type:Sequelize.DATE,
-        allowNull:false
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
       },
-      update_at:{
-        type:Sequelize.DATE,
-        allowNull:false
-      }
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable('users');
-  }
+  },
 };
