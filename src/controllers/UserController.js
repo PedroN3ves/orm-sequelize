7bab9df0bd2464d2a7eadc09c31ef36aff26731e
PedroN3ves <pedroholiveira784@gmail.com>
2023-09-20 10:16:17 -0300

    Get and Update Methods added

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,27 @@
 const User = require("../models/User");
 
 module.exports = {
+    async index(req, res){
+        const users = await User.findAll({raw: true});
+
+        return res.json(users);
+    },
+
     async store(req, res) {
         const { name, email } = req.body;
 
         const user = await User.create({name, email});
         return res.json(user);
+    },
+
+    async update(req, res){
+        const { id } = req.params;
+        const { name, email } = req.body;
+
+        const user = await User.update(
+            { name, email }, { where: {id: id} }
+        );
+
+        return res.json(user); 
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const UserController = require("./controllers/UserController");
 const  routes = express.Router();
 
 routes.post("/users", UserController.store);
+routes.get("/users", UserController.index);
 
 module.exports = routes;
