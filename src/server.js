f43910995cf0a7c753df90674d04890a9fbcbb2f
PedroN3ves <pedroholiveira784@gmail.com>
2023-09-20 08:20:16 -0300

    Update 1.2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
+const routes = require("./routes");
+
+require("./database/index")
+
 const port = 3333;
 
 const app = express();
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(port,() => console.log(`Server runnig on ${port}`));
